811c116383eb3c692c697871244f7fdf378bf54b
mdmohsin7 <59914433+mdmohsin7@users.noreply.github.com>
2020-06-28 13:42:15 +0530

    added speech to text functionality

--- lib/screens/stt_page.dart
+++ lib/screens/stt_page.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import '../sizeconfig.dart';
 
 class SpeechToTextPage extends StatefulWidget {
@@ -9,7 +14,41 @@ class SpeechToTextPage extends StatefulWidget {
 
 class _SpeechToTextPageState extends State<SpeechToTextPage> {
 
- String resultText;
+  bool _hasSpeech = false;
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String recognizedWords = "";
+  String lastError = "";
+  String lastStatus = "";
+  String _currentLocaleId = "";
+  List<LocaleName> _localeNames = [];
+  final SpeechToText speech = SpeechToText();
+
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechState();
+  }
+
+
+  Future<void> initSpeechState() async {
+    bool hasSpeech = await speech.initialize(
+        onError: errorListener, onStatus: statusListener);
+    if (hasSpeech) {
+      _localeNames = await speech.locales();
+
+      var systemLocale = await speech.systemLocale();
+      _currentLocaleId = systemLocale.localeId;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _hasSpeech = hasSpeech;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,33 +85,50 @@ class _SpeechToTextPageState extends State<SpeechToTextPage> {
                         child: Icon(Icons.cancel,color: Theme.of(context).accentColor,),
                         mini: true,
                         backgroundColor: Colors.white,
-                        onPressed: () {}
+                        onPressed: speech.isListening ? cancelListening : null,
                       ),
                       FloatingActionButton(
                           child: Icon(Icons.mic,color: Theme.of(context).accentColor,),
-                          mini: true,
+
                           backgroundColor: Colors.white,
-                          onPressed: () {}
+                          onPressed:
+                            !_hasSpeech || speech.isListening
+                                ? null
+                                : startListening,
+
                       ),
                       FloatingActionButton(
                           child: Icon(Icons.stop,color: Theme.of(context).accentColor,),
                           mini: true,
                           backgroundColor: Colors.white,
-                          onPressed: () {}
+                          onPressed: speech.isListening ? stopListening : null,
                       ),
                     ],
                   ),
                   Padding(
                     padding: const EdgeInsets.all(25.0),
-                    child: Container(
-                      height: SizeConfig.blockSizeHorizontal * 46,
+                    child: Container(padding: EdgeInsets.all(15.0),
+                      height: SizeConfig.blockSizeHorizontal * 42,
+                      width: SizeConfig.blockSizeVertical*45,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(25.0)),
                           color: Theme.of(context).backgroundColor,
                           boxShadow: [
                             BoxShadow(color: Colors.grey, blurRadius: 2.5),
                           ]),
+                      child: Text(recognizedWords),
                     )
+                  ),
+                  RaisedButton(
+                    child: Text('Clear',style: TextStyle(color: Theme.of(context).accentColor),),
+                    elevation: 2.4,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+                    onPressed: (){
+                      setState(() {
+                        recognizedWords = "";
+                      });
+                    },
                   ),
                 ],
               ),
@@ -82,4 +138,65 @@ class _SpeechToTextPageState extends State<SpeechToTextPage> {
       ),
     );
   }
+
+
+  void startListening() {
+    recognizedWords = "";
+    lastError = "";
+    speech.listen(
+        onResult: resultListener,
+        listenFor: Duration(seconds: 60),
+        localeId: _currentLocaleId,
+        onSoundLevelChange: soundLevelListener,
+        cancelOnError: true,
+        partialResults: true);
+    setState(() {});
+  }
+
+  void stopListening() {
+    speech.stop();
+    setState(() {
+      level = 0.0;
+    });
+  }
+
+  void cancelListening() {
+    speech.cancel();
+    setState(() {
+      level = 0.0;
+    });
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    setState(() {
+      recognizedWords = "${result.recognizedWords}";
+    });
+  }
+
+  void soundLevelListener(double level) {
+    minSoundLevel = min(minSoundLevel, level);
+    maxSoundLevel = max(maxSoundLevel, level);
+    //print("sound level $level: $minSoundLevel - $maxSoundLevel ");
+    setState(() {
+      this.level = level;
+    });
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    print("Received error status: $error, listening: ${speech.isListening}");
+    setState(() {
+      lastError = "${error.errorMsg} - ${error.permanent}";
+    });
+  }
+
+  void statusListener(String status) {
+    print(
+        "Received listener status: $status, listening: ${speech.isListening}");
+    setState(() {
+      lastStatus = "$status";
+    });
+  }
+
 }
+
+
