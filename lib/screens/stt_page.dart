a14430d96d41543500a2006230701ef664545207
mdmohsin7 <59914433+mdmohsin7@users.noreply.github.com>
2020-06-28 12:22:55 +0530

    removed SafeArea

--- lib/screens/stt_page.dart
+++ lib/screens/stt_page.dart
@@ -14,68 +14,71 @@ class _SpeechToTextPageState extends State<SpeechToTextPage> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: SizeConfig.blockSizeHorizontal * 20,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-              ),
-              child: Center(child: Text('Speech To Text',style: TextStyle(color: Theme.of(context).backgroundColor),)),
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: SizeConfig.blockSizeHorizontal * 25,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
             ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Theme.of(context).backgroundColor,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(45),
-                        topRight: Radius.circular(45))),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 50,),
-                    Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        FloatingActionButton(
-                          child: Icon(Icons.cancel,color: Theme.of(context).accentColor,),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: SizeConfig.blockSizeHorizontal * 14,),
+                Text('Speech To Text',style: TextStyle(color: Theme.of(context).backgroundColor,fontWeight: FontWeight.bold,fontSize: 24),),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(45),
+                      topRight: Radius.circular(45))),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 50,),
+                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FloatingActionButton(
+                        child: Icon(Icons.cancel,color: Theme.of(context).accentColor,),
+                        mini: true,
+                        backgroundColor: Colors.white,
+                        onPressed: () {}
+                      ),
+                      FloatingActionButton(
+                          child: Icon(Icons.mic,color: Theme.of(context).accentColor,),
                           mini: true,
                           backgroundColor: Colors.white,
                           onPressed: () {}
-                        ),
-                        FloatingActionButton(
-                            child: Icon(Icons.mic,color: Theme.of(context).accentColor,),
-                            mini: true,
-                            backgroundColor: Colors.white,
-                            onPressed: () {}
-                        ),
-                        FloatingActionButton(
-                            child: Icon(Icons.stop,color: Theme.of(context).accentColor,),
-                            mini: true,
-                            backgroundColor: Colors.white,
-                            onPressed: () {}
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(25.0),
-                      child: Container(
-                        height: SizeConfig.blockSizeHorizontal * 46,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                            color: Theme.of(context).backgroundColor,
-                            boxShadow: [
-                              BoxShadow(color: Colors.grey, blurRadius: 2.5),
-                            ]),
-                      )
-                    ),
-                  ],
-                ),
+                      ),
+                      FloatingActionButton(
+                          child: Icon(Icons.stop,color: Theme.of(context).accentColor,),
+                          mini: true,
+                          backgroundColor: Colors.white,
+                          onPressed: () {}
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Container(
+                      height: SizeConfig.blockSizeHorizontal * 46,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                          color: Theme.of(context).backgroundColor,
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, blurRadius: 2.5),
+                          ]),
+                    )
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
